6ad0cefe64853269cec49a0972e3ffb3b022eb19
Siddhant Kushwaha <k16.siddhant@gmail.com>
2019-01-28 22:01:00 +0530

    Improved dependency management.

--- sample/src/main/java/com/siddhantkushwaha/android/thugtools/sample/MainActivity.kt
+++ sample/src/main/java/com/siddhantkushwaha/android/thugtools/sample/MainActivity.kt
@@ -1,15 +1,13 @@
 package com.siddhantkushwaha.android.thugtools.sample
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.siddhantkushwaha.android.thugtools.thugtools.utility.GlideUtil
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-//        GlideUtil.preload(this@MainActivity, "")
     }
 }
